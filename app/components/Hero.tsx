2040f796607d825f3a03b702d0597c4ef2212278
flithgoo <prassetiyo2105@gmail.com>
2024-08-09 05:56:33 +0700

    update: hero section early development

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
-  return <div></div>;
+  return (
+    <section id="home" className="pt-36">
+      <div className="flex justify-center">
+        <div className="container flex flex-wrap">
+          <div className="max-w-lg self-center px-4 lg:w-1/2 text-green">
+            <h1 className="text-base font-semibold text-primary md:text-xl">
+              Selamat datang di website
+              <span className="mt-1 block text-4xl font-bold text-slate-900 lg:text-5xl">
+                PKBM Flamboyan
+              </span>
+            </h1>
+            <p className="mb-10 font-medium leading-relaxed text-secondary">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Provident nemo quia autem soluta voluptates temporibus aspernatur
+              recusandae reprehenderit in deleniti esse, voluptatum molestias
+              explicabo architecto perferendis. Aliquam eius aut quae.{" "}
+            </p>
+
+            <Link
+              href="#"
+              className="rounded-full bg-emerald-600 py-3 px-8 text-base font-semibold text-slate-100 transition duration-300 ease-in-out hover:opacity-80 hover:shadow-lg"
+            >
+              Daftar sekarang!
+            </Link>
+          </div>
+          <div className="w-full self-end px-4 lg:w-1/2">
+            <div className="mt-10 lg:right-0 lg:mt-9">
+              <Image
+                src="/heroimg/FOTO BERJAS.jpg"
+                alt="Sandhika Galih"
+                width={400}
+                height={600}
+                className="relative z-10 mx-auto max-w-full"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <div id="tentang-kami" className="h-96 bg-white">
-        <h1>halo</h1>
-      </div>
-      <div className="h-96 bg-neutral-400">
-        <h1>halo</h1>
-      </div>
-      <h1 id="home" className="h-lvh">
-        Hero
-      </h1>
+      <main className="bg-white">
+        <Hero />
+        <div id="tentang-kami" className="h-96 bg-white">
+          <h1>halo</h1>
+        </div>
+        <div className="h-96 bg-neutral-400">
+          <h1>halo</h1>
+        </div>
+        <h1 className="h-lvh"></h1>
+      </main>
     </>
   );
 }
